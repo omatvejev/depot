e49778981d71271231e4e38f3663a36a7f6c730e
Oleg Matvejev <omatveje@gmail.com>
2014-09-23 22:10:11 -0700

    Add Iteration F2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,6 +35,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         reset_counter
         format.html { redirect_to store_url }
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new', notice: 'Could not add product to the cart.' }
